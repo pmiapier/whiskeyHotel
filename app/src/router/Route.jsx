678a18463838cebe942c64a7a5720f444e74c206
PanaleePierce <panaleepierce@PanaleePierces-MacBook-Pro.local>
2023-10-10 11:43:38 +0700

    added Login.jsx and Booking.jsx

--- app/src/router/Route.jsx
+++ app/src/router/Route.jsx
@@ -1,21 +1,30 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Layout from '../layout/Layout';
-import Home from '../pages/Home';
-import Contact from '../pages/Contact';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "../layout/Layout";
+import Home from "../pages/Home";
+import Contact from "../pages/Contact";
+import Login from "../pages/Login";
+import Booking from "../pages/users/Booking";
 
 const router = createBrowserRouter([
-    {
-      path: '/',
-      element: (
-        <Layout />
-      ),
-      children: [
-        { path: '', element: <Home /> },
-        { path: '/contact', element: <Contact /> },
-      ]
-    }
-  ]);
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "/contact", element: <Contact /> },
+      { path: "/login", element: <Login /> },
+    ],
+  },
+  {
+    path: "/user",
+    element: <Layout />,
+    children: [
+      { path: "/user", element: <Home /> },
+      { path: "/user/booking", element: <Booking /> },
+    ],
+  },
+]);
 
 export default function Route() {
-    return <RouterProvider router={router} />;
-  }
+  return <RouterProvider router={router} />;
+}
