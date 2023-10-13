81c9270e273320c54ccd0eae4ae4888e2937577a
PanaleePierce <panaleepierce@PanaleePierces-MacBook-Pro.local>
2023-10-13 20:31:06 +0700

    took away the bg of the image on booking page

--- app/src/components/BookingImages.jsx
+++ app/src/components/BookingImages.jsx
@@ -6,18 +6,28 @@ export default function BookingImages() {
   return (
     <div className="grid gap-5 " style={{ width: "500px" }}>
       <div className="flex justify-start">
-        <div className="bg-gridMidnight w-64 h-64 rounded-lg shadow-md flex justify-center items-center">
-          <img src={cozy} className="w-60 h-60 rounded-lg" />
+        {/* bg-gridMidnight w-64 h-64 rounded-lg shadow-md flex justify-center items-center */}
+        <div>
+          <img
+            src={cozy}
+            className="w-60 h-60 rounded-xl border-gridMidnight border-8"
+          />
         </div>
       </div>
       <div className="flex justify-center">
-        <div className="bg-gridMidnight w-64 h-64 rounded-lg shadow-md flex justify-center items-center">
-          <img src={chillout} className="w-60 h-60 rounded-lg" />
+        <div>
+          <img
+            src={chillout}
+            className="w-60 h-60 rounded-xl border-gridMidnight border-8"
+          />
         </div>
       </div>
       <div className="flex justify-end">
-        <div className="bg-gridMidnight w-64 h-64 rounded-lg shadow-md flex justify-center items-center">
-          <img src={party} className=" w-60 h-60 rounded-lg" />
+        <div>
+          <img
+            src={party}
+            className=" w-60 h-60 rounded-xl border-gridMidnight border-8"
+          />
         </div>
       </div>
     </div>
