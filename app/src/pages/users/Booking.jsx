b7fc636cf5379fc705d4f4edaf5c2159db2cc12b
PanaleePierce <panaleepierce@PanaleePierces-MacBook-Pro.local>
2023-10-15 12:50:41 +0700

    I dont remember what I added

--- app/src/pages/users/Booking.jsx
+++ app/src/pages/users/Booking.jsx
@@ -3,7 +3,7 @@ import BookingImages from "../../components/BookingImages";
 
 export default function Booking() {
   return (
-    <div className="flex justify-center gap-20 bg-offWhite py-40">
+    <div className="flex justify-center gap-20 bg-offWhite py-20">
       <BookingDetails />
 
       <BookingImages />
