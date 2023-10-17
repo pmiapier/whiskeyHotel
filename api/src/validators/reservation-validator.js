0d35a0544a8c96960971acf6d6cc1335e70e1ead
pmiapier <pmiapier@proton.me>
2023-10-17 09:39:50 +0400

    Reservations controller

---
+++ api/src/validators/reservation-validator.js
@@ -0,0 +1,22 @@
+const Joi = require('joi');
+
+const reservationSchema = Joi.object({
+  room_id: Joi.number().integer().positive().required(),
+  check_in_date: Joi.date(),
+  check_out_date: Joi.date()
+});
+exports.reservationSchema = reservationSchema;
+
+const getReservationSchema = Joi.object({
+  check_in_date: Joi.date(),
+  check_out_date: Joi.date()
+});
+
+exports.getReservationSchema = getReservationSchema;
+
+const roomSchema = Joi.object({
+  id: Joi.number().integer().positive().required(),
+  isMaintaining: Joi.boolean().required()
+});
+
+exports.roomSchema = roomSchema;
