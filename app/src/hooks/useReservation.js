3ae642318c20f56dfcf00bfff2d5dfebb6dd1a06
PanaleePierce <panaleepierce@PanaleePierces-MacBook-Pro.local>
2023-10-23 21:13:28 +0700

    added admin feature so that the admin call get all the reservations

--- app/src/hooks/useReservation.js
+++ app/src/hooks/useReservation.js
@@ -1,50 +1,72 @@
-import { useState } from 'react';
-import axios from 'axios';
-
+import { useState } from "react";
+import axios from "axios";
 
 export function useReservation() {
-    const [checkinDate, setCheckinDate] = useState();
-    const [checkoutDate, setCheckoutDate] = useState();
-    const [roomsAvailable, setRoomsAvailable] = useState({});
+  const [checkinDate, setCheckinDate] = useState();
+  const [checkoutDate, setCheckoutDate] = useState();
+  const [roomsAvailable, setRoomsAvailable] = useState({});
+  const [reservations, setReservations] = useState([]);
 
-    const getRoomAvailability = async () => {
-      const reservationData = {
-        check_in_date: checkinDate,
-        check_out_date: checkoutDate,
-      };
-      const res = await axios.post('/reservation/get-room-availability', reservationData);
-      setRoomsAvailable(res.data.roomsAvailable)
-    }
+  const getRoomAvailability = async () => {
+    const reservationData = {
+      check_in_date: checkinDate,
+      check_out_date: checkoutDate,
+    };
+    const res = await axios.post(
+      "/reservation/get-room-availability",
+      reservationData
+    );
+    setRoomsAvailable(res.data.roomsAvailable);
+  };
 
-    const reserveRoom = async (roomType, checkinDate, checkoutDate) => {
-      const reservationData = {
-        room_type: roomType,
-        check_in_date: checkinDate,
-        check_out_date: checkoutDate,
-      };
-      const res = await axios.post('/reservation/create-reservation', reservationData);
-      console.log(res.data)
-    }
+  const reserveRoom = async (roomType, checkinDate, checkoutDate) => {
+    const reservationData = {
+      room_type: roomType,
+      check_in_date: checkinDate,
+      check_out_date: checkoutDate,
+    };
+    const res = await axios.post(
+      "/reservation/create-reservation",
+      reservationData
+    );
+    console.log(res.data);
+  };
 
-    const changeReservation = async (roomType, checkinDate, checkoutDate, reservationID) => {
-      const reservationData = {
-        room_type: roomType,
-        check_in_date: checkinDate,
-        check_out_date: checkoutDate,
-        id: reservationID,
-      };
-      const res = await axios.post('/reservation/change-reservation', reservationData);
-      console.log(res.data)
-    }
+  const changeReservation = async (
+    roomType,
+    checkinDate,
+    checkoutDate,
+    reservationID
+  ) => {
+    const reservationData = {
+      room_type: roomType,
+      check_in_date: checkinDate,
+      check_out_date: checkoutDate,
+      id: reservationID,
+    };
+    const res = await axios.post(
+      "/reservation/change-reservation",
+      reservationData
+    );
+    console.log(res.data);
+  };
 
-    return {
-      checkinDate,
-      setCheckinDate,
-      checkoutDate,
-      setCheckoutDate,
-      roomsAvailable,
-      getRoomAvailability,
-      reserveRoom,
-      changeReservation,
-    }
+  // const getAllReservations = async () => {
+  //   const res = await axios.get("reservation/get-all-reservations");
+  //   console.log(res.data);
+  //   setReservations(res.data);
+  // };
+
+  return {
+    checkinDate,
+    setCheckinDate,
+    checkoutDate,
+    setCheckoutDate,
+    roomsAvailable,
+    getRoomAvailability,
+    reserveRoom,
+    changeReservation,
+    // getAllReservations,
+    reservations,
+  };
 }
--- app/src/pages/admins/AdminManageBooking.jsx
+++ app/src/pages/admins/AdminManageBooking.jsx
@@ -2,7 +2,7 @@ import AdminBookingList from "../../components/reservation/AdminBookingList";
 
 export default function AdminManageBooking() {
   return (
-    <div className=" bg-offWhite px-96 py-20  ">
+    <div className="flex bg-offWhite rounded-lg px-10 py-10 ">
       <AdminBookingList />
     </div>
   );
